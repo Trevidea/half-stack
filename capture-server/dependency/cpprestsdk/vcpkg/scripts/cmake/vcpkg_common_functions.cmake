3ab29dde997f63bcc1d31914cac5a813088b0474
Manish Verma <manish@drake.in>
2024-03-16 12:49:14 +0530

    deleted the dependency folders and other changes

--- capture-server/dependency/cpprestsdk/vcpkg/scripts/cmake/vcpkg_common_functions.cmake
+++
@@ -1,3 +0,0 @@
-# DEPRECATED
-
-message("${_VCPKG_BACKCOMPAT_MESSAGE_LEVEL}" "vcpkg_common_functions has been removed and all values are automatically provided in all portfile.cmake invocations. Please remove `include(vcpkg_common_functions)`.")
