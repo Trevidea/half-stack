6dc3527dd57c11ddd68980af3b8bccace5f23c71
Sanjay Singh Rawat <sanjay@drake.co.in>
2024-03-19 19:13:10 +0530

    refactoring event html  code

--- capture-server-ui/src/app/sport-pip-capture/components/event/on-going-event/on-going-event.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/event/on-going-event/on-going-event.component.ts
@@ -1,35 +1,15 @@
-import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { slideInFromLeft, slideInFromTop, slideInFromRight, slideIn } from './animations';
 @Component({
   selector: 'app-on-going-event',
   templateUrl: './on-going-event.component.html',
   styleUrls: ['./on-going-event.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  animations: [slideInFromLeft, slideInFromTop, slideInFromRight, slideIn,
-
-
-  ],
-
-
-  // animations: [
-  //   trigger('fadeInOut', [
-  //     transition(':enter', [
-  //       style({ opacity: 0 }),
-  //       animate('300ms', style({ opacity: 1 })),
-  //     ]),
-  //     transition(':leave', [
-  //       animate('300ms', style({ opacity: 0 })),
-  //     ]),
-  //   ]),
-  // ],
-
 })
 export class OnGoingEventComponent implements OnInit {
   @Input() datasource: any
-
   public selectBasic: any[] = [];
   public selectBasicLoading = false;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -43,14 +23,10 @@ export class OnGoingEventComponent implements OnInit {
   }
 
   updateCountdownTimers() {
-
     const currentDateTime = this.getCurrentDateTime();
-
     this.datasource.forEach((event, index) => {
-
       const eventDateTime = new Date(event.dttEvent + 'T' + event.time);
       const timeDifference = currentDateTime.getTime() - eventDateTime.getTime();
-
       if (timeDifference > 0) {
         const hours = Math.floor(timeDifference / (1000 * 60 * 60));
         const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
@@ -58,7 +34,7 @@ export class OnGoingEventComponent implements OnInit {
 
         event.countdown = `${hours}h ${minutes}m ${seconds}s`
       } else {
-       
+
         event.countdown = null;
       }
     });
