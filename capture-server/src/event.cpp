fc87879e1cad4868d690db56308ab1d67dc8c365
Shrerya <shreya@drake.co.in>
2024-03-20 17:02:27 +0530

    event class

--- capture-server/src/event.cpp
+++ capture-server/src/event.cpp
@@ -10,13 +10,71 @@ void Event::report()
     // Route definitions
     Gateway::instance().route("GET", "/api/events", [this](const Request &req, Response &rsp)
                               { this->listUpcoming(req, rsp); });
-
 }
 
 void Event::listUpcoming(const Request &req, Response &rsp)
 {
     // Set the response body with the JSON data
-    rsp.setData(R"("result":[{"name":"shreya"}],"count":1)");
+    rsp.setData(R"V0G0N(" {
+        "Absolute URI" : "/api/event/template",
+                         "Gateway Response" : [
+                             {
+                                 "field" : "id",
+                                 "type" : 1,
+                                 "value" : "Unknown"
+                             },
+                             {
+                                 "field" : "year",
+                                 "type" : 1,
+                                 "value" : "Unknown"
+                             },
+                             {
+                                 "field" : "dttevent",
+                                 "type" : 1,
+                                 "value" : "Unknown"
+                             },
+                             {
+                                 "field" : "venue",
+                                 "type" : 1,
+                                 "value" : "Unknown"
+                             },
+                             {
+                                 "field" : "onpremise",
+                                 "type" : 1,
+                                 "value" : true
+                             },
+                             {
+                                 "field" : "detail",
+                                 "type" : 1,
+                                 "value" : "Unknown"
+                             },
+                             {
+                                 "field" : "sport",
+                                 "type" : 1,
+                                 "value" : "var-char"
+                             },
+                             {
+                                 "field" : "level",
+                                 "type" : 1,
+                                 "value" : "var-char"
+                             },
+                             {
+                                 "field" : "program",
+                                 "type" : 1,
+                                 "value" : "var-char"
+                             },
+                             {
+                                 "field" : "status",
+                                 "type" : 1,
+                                 "value" : "var-char"
+                             },
+                             {
+                                 "field" : "title",
+                                 "type" : 1,
+                                 "value" : "var-char"
+                             }
+                         ]
+    } ")V0G0N");
     // Set appropriate headers and status code
     rsp.complete();
 }
