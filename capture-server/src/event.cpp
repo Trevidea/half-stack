8da5ca969920a31d2ca36e0cd16e6cf0eae7e6b5
Shrerya <shreya@drake.co.in>
2024-03-21 12:57:00 +0530

    commit

--- capture-server/src/event.cpp
+++ capture-server/src/event.cpp
@@ -16,7 +16,7 @@ void Event::listUpcoming(const Request &req, Response &rsp)
 {
     // Set the response body with the JSON data
     rsp.setData(R"V0G0N(" {
-        "Absolute URI" : "/api/event/",
+        "Absolute URI" : "/api/event/template",
                          "Gateway Response" : [
                              {
                                  "field" : "id",
