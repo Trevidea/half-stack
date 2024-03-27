57fd827a9a765c73483405cb0aeefeba217a510b
rakesh <rakesh.ranjan@drake.co.in>
2024-03-27 16:22:06 +0530

    added previous connection using new figma design

---
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-data.ts
@@ -0,0 +1,58 @@
+// Define array of objects
+export const PreviousEventsConnection = [
+  {
+    id: 1,
+    "Name of event": "CSK VS Mumbai Indians ",
+    date: "2024-03-27",
+    "Total connection": 100,
+    Duration: "2 hours",
+    "Most connected device": [
+      { device: "sub", time: "2hr 00min" },
+      { device: "pub", time: "0hr 10min" },
+    ],
+  },
+  {
+    id: 2,
+    "Name of event": "Sunriser VS GT",
+    date: "2024-04-15",
+    "Total connection": 150,
+    Duration: "3 hours",
+    "Most connected device": [
+      { device: "sub", time: "1hr 30min" },
+      { device: "pub", time: "1hr 30min" },
+    ],
+  },
+  {
+    id: 3,
+    "Name of event": "PBKS VS RCB",
+    date: "2024-05-01",
+    "Total connection": 80,
+    Duration: "1.5 hours",
+    "Most connected device": [
+      { device: "sub", time: "1hr" },
+      { device: "pub", time: "0hr 30min" },
+    ],
+  },
+  {
+    id: 4,
+    "Name of event": "GT VS MI",
+    date: "2024-05-15",
+    "Total connection": 120,
+    Duration: "2 hours",
+    "Most connected device": [
+      { device: "sub", time: "1hr 45min" },
+      { device: "pub", time: "0hr 15min" },
+    ],
+  },
+  {
+    id: 5,
+    "Name of event": "RR VS LSG",
+    date: "2024-06-01",
+    "Total connection": 90,
+    Duration: "2 hours",
+    "Most connected device": [
+      { device: "sub", time: "1hr 45min" },
+      { device: "pub", time: "0hr 15min" },
+    ],
+  },
+];
--- capture-server-ui/src/app/sport-pip-capture/components/connection/previous-events-connection/previous-events-connection.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/previous-events-connection/previous-events-connection.component.ts
@@ -1,31 +1,56 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
-import { PreviousEventsConnection } from './data'
-import { DataFactoryService } from 'app/sport-pip-capture/models/data-factory.service';
-import { error } from 'console';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
+import { ColumnMode, SelectionType } from "@swimlane/ngx-datatable";
+import { PreviousEventsConnection } from "./data";
+import { DataFactoryService } from "app/sport-pip-capture/models/data-factory.service";
+
 @Component({
-  selector: 'app-previous-events-connection',
-  templateUrl: './previous-events-connection.component.html',
-  styleUrls: ['./previous-events-connection.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-previous-events-connection",
+  templateUrl: "./previous-events-connection.component.html",
+  styleUrls: ["./previous-events-connection.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PreviousEventsConnectionComponent implements OnInit {
-  @Input() datasource: any
+  @Input() datasource: any;
+  @Input() previousEventsConnection: any;
   public ColumnMode = ColumnMode;
   public SelectionType = SelectionType;
+  sortAscending: boolean = true;
   private tempData = [];
   public selected = [];
   public rows: any;
-  constructor(private dataFactory: DataFactoryService) { }
+  constructor(
+    private dataFactory: DataFactoryService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-
-
-    this.dataFactory.read('connection-with-past-details').subscribe((res: any) => {
-      // console.log(res)
-      this.rows = res
-    }, error => {
-      console.log(error)
-    })
+    this.dataFactory.read("connection-with-past-details").subscribe(
+      (res: any) => {
+        // console.log(res)
+        this.rows = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  sortDate() {
+    console.log("clicked");
+    this.sortAscending = !this.sortAscending;
+    this.previousEventsConnection.sort((a, b) => {
+      if (this.sortAscending) {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      } else {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+      }
+    });
   }
 }
