aa097dce2ec201d4ab198074c5fad18e39e7546b
rakesh <rakesh.ranjan@drake.co.in>
2024-03-30 01:16:26 +0530

    added table for live streaming connection

--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-start/connection-start.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-start/connection-start.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { header } from './data'
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { header } from "./data";
+import { EventConnection } from "../connection-data";
 @Component({
-  selector: 'app-connection-start',
-  templateUrl: './connection-start.component.html',
-  styleUrls: ['./connection-start.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-connection-start",
+  templateUrl: "./connection-start.component.html",
+  styleUrls: ["./connection-start.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ConnectionStartComponent implements OnInit {
-  header: string[]
-  constructor() { }
-  connectiondetail: boolean = false
+  header: string[];
+  eventConnection: any;
+  constructor() {
+    this.eventConnection = EventConnection;
+  }
+  connectiondetail: boolean = false;
   ngOnInit(): void {
-    this.header = header
+    this.header = header;
   }
   ConnectionDetails(yes: boolean) {
-    this.connectiondetail = yes
+    this.connectiondetail = yes;
   }
-  closeDetail(){
-    this.connectiondetail = false
+  closeDetail() {
+    this.connectiondetail = false;
   }
 }
