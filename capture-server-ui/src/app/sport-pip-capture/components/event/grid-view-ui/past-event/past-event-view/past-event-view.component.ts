e3dd344e3be5e71eba0e9c872243a19bcdc15367
Sanjay Singh Rawat <sanjay@drake.co.in>
2024-04-08 11:17:12 +0530

    used hls model player in pass event vioew

--- capture-server-ui/src/app/sport-pip-capture/components/event/grid-view-ui/past-event/past-event-view/past-event-view.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/event/grid-view-ui/past-event/past-event-view/past-event-view.component.ts
@@ -8,7 +8,9 @@ import { PlyrComponent } from 'ngx-plyr';
   encapsulation: ViewEncapsulation.None
 })
 export class PastEventViewComponent implements OnInit {
-
+  // url = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+  // url = "http://drake.in:59919/spip_school_stream/ind_vs_pak/llhls.m3u8";
+  url = "assets/videos/2/output.m3u8";
   // get the component instance to have access to plyr instance
   @ViewChild(PlyrComponent)
 
@@ -20,47 +22,47 @@ export class PastEventViewComponent implements OnInit {
 
   ngOnInit(): void {
   }
- // video Sources
- public videoSources: Plyr.Source[] = [
-  {
-    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
-    type: 'video/mp4',
-    size: 576
-  },
-  {
-    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
-    type: 'video/mp4',
-    size: 720
-  },
-  {
-    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
-    type: 'video/mp4',
-    size: 1080
-  },
-  {
-    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1440p.mp4',
-    type: 'video/mp4',
-    size: 1440
-  }
-];
+  // video Sources
+  public videoSources: Plyr.Source[] = [
+    {
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4',
+      type: 'video/mp4',
+      size: 576
+    },
+    {
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+      type: 'video/mp4',
+      size: 720
+    },
+    {
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+      type: 'video/mp4',
+      size: 1080
+    },
+    {
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1440p.mp4',
+      type: 'video/mp4',
+      size: 1440
+    }
+  ];
 
-public tracks = [
-  {
-    kind: 'captions',
-    label: 'English',
-    srclang: 'en',
-    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt',
-    default: true
-  },
-  {
-    kind: 'captions',
-    label: 'French',
-    srclang: 'fr',
-    src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt'
-  }
-];
+  public tracks = [
+    {
+      kind: 'captions',
+      label: 'English',
+      srclang: 'en',
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.en.vtt',
+      default: true
+    },
+    {
+      kind: 'captions',
+      label: 'French',
+      srclang: 'fr',
+      src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.fr.vtt'
+    }
+  ];
 
-public poster = 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg';
+  public poster = 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg';
 
 
 
