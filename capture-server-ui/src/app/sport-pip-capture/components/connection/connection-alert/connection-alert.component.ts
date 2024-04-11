938e7dc6bf687a4d09f6d9929ce53106ed2bed8f
rakesh <rakesh.ranjan@drake.co.in>
2024-04-11 15:25:38 +0530

    commit

--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-alert/connection-alert.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-alert/connection-alert.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { EventUndoNotificationComponent } from "./event-undo-notification/event-undo-notification.component";
 
 @Component({
   selector: "app-connection-alert",
@@ -10,11 +11,46 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 export class ConnectionAlertComponent implements OnInit {
   @Input() title: string;
   @Input() description: string;
+  undoEvent: boolean = true;
   constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {}
 
   close() {
     this.modalService.dismissAll();
+    if (this.title == "End Event") {
+      const undoNotification = this.modalService.open(
+        EventUndoNotificationComponent,
+        {
+          centered: false,
+          size: "sm",
+          windowClass: "event-notification-undo",
+        }
+      );
+      undoNotification.componentInstance.undoEvent = false;
+
+      undoNotification.result.then(
+        (selectedItems) => {
+          this.undoEvent = selectedItems;
+          console.log("Parent :::", this.undoEvent, selectedItems);
+        },
+        (reason) => {}
+      );
+    }
   }
 }
+/*
+ const modalRef = this.modalService.open(FormListModalComponent, {
+      size: "lg",
+    });
+
+    modalRef.componentInstance.items = this.peoples;
+    modalRef.componentInstance.type = "Peoples";
+
+    modalRef.result.then(
+      (selectedItems) => {
+        this.datasource.peoples = selectedItems;
+      },
+      (reason) => {}
+    );
+*/
--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-alert/event-undo-notification/event-undo-notification.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-alert/event-undo-notification/event-undo-notification.component.ts
@@ -1,5 +1,12 @@
-import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation,
+} from "@angular/core";
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-event-undo-notification",
@@ -10,11 +17,20 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 export class EventUndoNotificationComponent implements OnInit {
   @Input() title: string;
   @Input() description: string;
-  constructor(private modalService: NgbModal) {}
+  @Input() undoEvent: boolean;
+  @Output() selectedItems = new EventEmitter<any>();
+  constructor(
+    private modalService: NgbModal,
+    public activeModal: NgbActiveModal
+  ) {}
 
   ngOnInit(): void {}
 
   close() {
     this.modalService.dismissAll();
+  }
+  undo() {
+    this.undoEvent = true;
+    this.activeModal.close(this.undoEvent);
   }
 }
