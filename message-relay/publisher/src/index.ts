975cb85299b292885ae42f756d0be28d51b98456
rakesh <rakesh.ranjan@drake.co.in>
2024-04-12 11:31:01 +0530

    added statick data inside massage relay Publisher for testing data

--- message-relay/publisher/src/index.ts
+++ message-relay/publisher/src/index.ts
@@ -9,7 +9,7 @@ const connectionType: string = process.env.connection_type || 'tcp';
 // Define the host. Default is to publish on all.
 const host: string = process.env.host || '*';
 // Set the topic of the stream
-const topicfilter: string = process.env.topic || 'rand';
+const topicfilter: string = process.env.topic || 'event';
 const Publishport: string = `${connectionType}://${host}:${port}`;
 
 // Create a ZeroMQ publisher socket
@@ -21,10 +21,42 @@ const subscriber: zmq.Subscriber = new zmq.Subscriber();
   console.log(`Sending on topic: ${topicfilter}`);
 })();
 
-// Function to send a random number every second
-setInterval(() => {
-  // Create a random number
-  const msg = { num: 'abc' + crypto.randomInt(0, 10) };
-  // Send this message
-  socket.send([topicfilter, JSON.stringify(msg)]);
-}, 1000);
+function sendEvent() {
+  const startEvent = {
+    id: 1,
+    dtEvent: '2024-12-04',
+    level: 'Diversity',
+    program: 'Man',
+    sport: 'Cricket',
+    status: 'OK',
+    title: 'CSK Vs MI',
+    detail: {
+      cityAddress: 'Mohali',
+      streetAddress: '12 Kb 18',
+      type: 'On Demand Event',
+    },
+    year: 2024,
+    venue: {
+      location: 'b24,vikas nagar Ludhiana',
+    },
+    time: 1211,
+    type: 'On Demand Event',
+    countdown: '111',
+    activeDevice: [
+      { id: 1, deviceName: 'oppo hand cam' },
+      { id: 2, deviceName: 'iphone 15 Max pro' },
+    ],
+  };
+  socket.send([topicfilter, JSON.stringify(startEvent)]);
+}
+setTimeout(() => {
+  sendEvent();
+  setTimeout(() => {
+    const msg = { eventStatus: 'Event Stopped' };
+    socket.send([topicfilter, JSON.stringify(msg)]);
+  }, 10000);
+  setTimeout(() => {
+    const preview = { eventStatus: 'Event Ready for Preview' };
+    socket.send([topicfilter, JSON.stringify(preview)]);
+  }, 20000);
+}, 4000);
