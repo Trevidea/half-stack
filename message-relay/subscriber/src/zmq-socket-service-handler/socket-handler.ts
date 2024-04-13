6eabc077a259569668d7e5feecd7362916015392
rakesh <rakesh.ranjan@drake.co.in>
2024-04-13 12:35:58 +0530

    commit

--- message-relay/subscriber/src/zmq-socket-service-handler/socket-handler.ts
+++ message-relay/subscriber/src/zmq-socket-service-handler/socket-handler.ts
@@ -4,7 +4,7 @@ import { ZmqMessageHandler } from './zmq-message-handler';
 export class SocketHandler {
   static connectSocket(topic: any, message: any) {
     SocketService.io.on('connection', (socket: any) => {
-      SocketService.io.emit('event', `${message.toString()}`);
+      SocketService.io.emit(`${topic}`, `${message.toString()}`);
     });
   }
 }
