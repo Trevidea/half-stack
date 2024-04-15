deaf101e55ecca8302126b279891f668c2cc2195
rakesh <rakesh.ranjan@drake.co.in>
2024-04-15 10:27:16 +0530

    commit

--- message-relay/subscriber/src/index.ts
+++ message-relay/subscriber/src/index.ts
@@ -1,10 +1,30 @@
-import { Server } from './zmq-socket-service-handler/server';
-import { Server as Http } from 'http';
-const server = new Server();
+import { Server } from 'socket.io';
+import http from 'http';
 
-const app = server.app;
+const httpServer = http.createServer();
 
-export const myServer: Http = app.listen(5000, () => {
-  server.init();
-  console.log('running 5000');
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:4200',
+  },
 });
+httpServer.listen(3000);
+console.log('Relay service running on port 3000');
+
+async function run() {
+  const zmq = require('zeromq');
+  const sock = new zmq.Subscriber();
+
+  sock.connect('tcp://127.0.0.1:4001');
+  sock.subscribe('event-terminal');
+  sock.subscribe('event-preview');
+  sock.subscribe('live-event');
+  console.log('Subscriber connected to port 4001');
+
+  for await (const [topic, msg] of sock) {
+    console.log('Topic:', topic.toString(), 'message:', msg.toString());
+    io.emit(topic.toString(), msg.toString());
+  }
+}
+
+run();
