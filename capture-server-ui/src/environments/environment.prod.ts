c5225f95a07ffabe4e7e438537f1e7efbfa94eb1
Manish Verma <manish@drake.in>
2024-04-16 19:40:24 +0530

    prod ebvironment

--- capture-server-ui/src/environments/environment.prod.ts
+++ capture-server-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   hmr: false,
   apiUrl: 'http://localhost:3000/api',
-  spModelUrl:'http://localhost:1437/api',
+  spModelUrl:'https://hr.api.drake.in/api',
   spRelayUrl:'http://localhost:3001',
   spFSUrl:'http://localhost:1337/api',
   spHLSUrl:'http://localhost:59919',
