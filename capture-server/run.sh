beb9563546c8bc1b2886485c154894da07e4e0a0
Manish Verma <manish@drake.in>
2024-04-16 12:08:16 +0530

    environ ment vars

---
+++ capture-server/run.sh
@@ -0,0 +1,8 @@
+export PG_SRV="drake.in"
+export PG_DBS="half-stack"
+export PG_USR="postgres"
+export PG_PWD="btc.008"
+export PG_PORT="52525"
+export FS_URL="http://drake.in:1337/api"
+# Run the service
+./build/api/capture-server
