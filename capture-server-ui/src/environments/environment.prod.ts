1de8c75deaf83a87239e13b2673b63a3d06eccc4
Manish Verma <manish@drake.in>
2024-04-17 08:12:52 +0530

    environment vars for prod

--- capture-server-ui/src/environments/environment.prod.ts
+++ capture-server-ui/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   hmr: false,
   apiUrl: 'http://localhost:3000/api',
-  spModelUrl:'https://hr.api.drake.in/api',
-  spRelayUrl:'https://sp-ipad.site',
-  spFSUrl:'http://localhost:1337/api',
-  spHLSUrl:'http://localhost:59919',
+  spModelUrl:'https://api.sp-halfstack.site/api',
+  spRelayUrl:'https://relay.sp-halfstack.site',
+  spFSUrl:'https://strapi.sp-fullstack.site/api',
+  spHLSUrl:'https://dumps.sp-halfstack.site',
 };
