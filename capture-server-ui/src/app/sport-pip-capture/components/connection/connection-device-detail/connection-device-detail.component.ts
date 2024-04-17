0ac348b2d75583828b7b1fe17a6b6cdb49e61ff4
rakesh <rakesh.ranjan@drake.co.in>
2024-04-17 11:44:21 +0530

    commit

--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/connection-device-detail.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/connection-device-detail.component.ts
@@ -7,8 +7,8 @@ import { environment } from "environments/environment";
   styleUrls: ["./connection-device-detail.component.scss"],
 })
 export class ConnectionDeviceDetailComponent implements OnInit {
-  url = "assets/videos/2/output.m3u8";
-  // url = `${environment.spHLSUrl}/spip_school_stream/ind_vs_pak/llhls.m3u8`;
+  // url = "assets/videos/2/output.m3u8";
+  url = `https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8`;
   constructor(private router: Router, private route: ActivatedRoute) {}
   listOrGrid: string;
   ngOnInit(): void {
