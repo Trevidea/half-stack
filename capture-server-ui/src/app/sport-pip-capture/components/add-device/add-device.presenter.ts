1f1fd5a54ea0e943834a4a2d7c50c8c1cff66b98
Sanjay Singh Rawat <sanjay@drake.co.in>
2024-05-06 11:35:02 +0530

    chnages made in add event device component and connection component

--- capture-server-ui/src/app/sport-pip-capture/components/add-device/add-device.presenter.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/add-device/add-device.presenter.ts
@@ -36,15 +36,23 @@ export class AddDevicePresenter implements OnInit {
   ) {
     this.ds = new AddDeviceView();
     this.options = this.toastr.toastrConfig;
-
-    this.actions = new ModalActions(this.ds, this.modelServiceService.saveDevice, AddDeviceBuilder, this.onClose)
+    this.actions = new PresenterAction('', this.ds, this.modelServiceService.saveDevice, AddDeviceBuilder, router);
     this.actions.onComplete.subscribe((result) => {
+      console.log(result);
       if (result) {
         this.toastrSuccess()
       } else {
         this.toastrError()
       }
     });
+    // this.actions = new ModalActions(this.ds, this.modelServiceService.saveDevice, AddDeviceBuilder, this.onClose)
+    // this.actions.onComplete.subscribe((result) => {
+    //   if (result) {
+    //     this.toastrSuccess()
+    //   } else {
+    //     this.toastrError()
+    //   }
+    // });
 
     Transformer.ComposeCollectionViewAsync(this.modelServiceService.userJson(), this.ds.userName,
       (userItem: UserProfileData) => {
