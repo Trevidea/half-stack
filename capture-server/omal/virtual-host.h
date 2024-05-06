45b014d7fcaf17626ebd05cac42c33201441434a
Manish Verma <manish@drake.in>
2024-05-06 13:51:32 +0530

    removed unused code from virtual-host

--- capture-server/omal/virtual-host.h
+++ capture-server/omal/virtual-host.h
@@ -18,22 +18,8 @@ public:
     // const char *DUMPS_BASE_LOCATION = "/usr/share/ovenmediaengine/conf/html";
     friend class OMALFactory;
 
-    /// @brief Creates a stream in the specified application with the key provided by the function user
-    /// @param app The name of the application within which the stream has to be created
-    /// @param key The key to be used to create the stream, that shall be used by the RTMP device
-    /// @param profile The profile to be created for the stream, to be used for encoding and other recording configurations
-    /// @return returns the stream-key that the device will use
-    std::string createStream(const std::string &app, const std::string &key, const OutputProfile &profile);
-
-    /// @brief Gets the locations of all the VOD dumps know to the OMAL
-    /// @return a collection
     std::map<std::string, std::string> getVODDumps();
 
-    /// @brief https://airensoft.gitbook.io/ovenmediaengine/streaming/low-latency-hls#dump
-    /// @param streamName
-    /// @param relativeOutputPath
-    /// @return
-    void setVODDumps(const std::string &streamName, const std::string &relativeOutputPath);
     Json::Value deepFindOrCreate();
     Json::Value createApp(const std::string &app);
 
