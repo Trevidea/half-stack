efbc3f3258b20f27fcd741b268932405ab57f393
rakesh <rakesh.ranjan@drake.co.in>
2024-05-08 21:28:10 +0530

    added dark-layout with relay-stream component

--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/relay-stream/relay-stream.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/relay-stream/relay-stream.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: "app-relay-stream",
   templateUrl: "./relay-stream.component.html",
   styleUrls: ["./relay-stream.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class RelayStreamComponent implements OnInit {
   constructor(public activeModal: NgbActiveModal) {}
