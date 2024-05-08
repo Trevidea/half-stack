3a6e88d1030b784392faf6e8db503cc27be23386
rakesh <rakesh.ranjan@drake.co.in>
2024-05-08 13:42:07 +0530

    added validation with OM-form

--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/oven-media-server-form/oven-media-server-form.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/oven-media-server-form/oven-media-server-form.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, OnInit } from "@angular/core";
+import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: "app-oven-media-server-form",
   templateUrl: "./oven-media-server-form.component.html",
   styleUrls: ["./oven-media-server-form.component.scss"],
 })
 export class OvenMediaServerFormComponent implements OnInit {
-  constructor(public activeModal: NgbActiveModal) {}
+  @Input() datasource: any;
+  constructor(
+    public activeModal: NgbActiveModal,
+    public modalService: NgbModal
+  ) {}
   ngOnInit(): void {}
 }
