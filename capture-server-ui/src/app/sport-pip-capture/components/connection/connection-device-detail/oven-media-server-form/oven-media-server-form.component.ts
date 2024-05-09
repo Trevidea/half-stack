834eb663e28d88d56574d5761da84705024386cb
rakesh <rakesh.ranjan@drake.co.in>
2024-05-09 13:42:15 +0530

    commit created new module for inlineform

--- capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/oven-media-server-form/oven-media-server-form.component.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/connection/connection-device-detail/oven-media-server-form/oven-media-server-form.component.ts
@@ -21,17 +21,49 @@ export class OvenMediaServerFormComponent implements OnInit {
       const control = form.controls[key];
       if (control.invalid) {
         const inputElement = document.getElementsByName(key)[0] as HTMLElement;
-        inputElement.style.borderColor = "red";
+        inputElement.style.border = "1px solid red";
       } else {
         const inputElement = document.getElementsByName(key)[0] as HTMLElement;
-        inputElement.style.borderColor = "";
+        inputElement.style.border = "1px solid #d8d6de";
       }
     });
   }
   onConfirm() {
     const formControls = document.querySelectorAll(".form-control");
     formControls.forEach((control) => {
-      (control as HTMLElement).style.borderColor = "";
+      (control as HTMLElement).style.border = "1px solid #d8d6de";
     });
   }
 }
+
+/*
+
+  formValidation(form: NgForm) {
+    Object.keys(form.controls).forEach((key) => {
+      const control = form.controls[key];
+      const inputElement = document.getElementsByName(key)[0] as HTMLElement;
+      if (control.invalid) {
+        inputElement.style.border = "1px solid red";
+        // Check if the warning icon already exists, if not, create and append it
+        if (!inputElement.querySelector(".warning-icon")) {
+          const warningIcon = document.createElement("i");
+          warningIcon.classList.add(
+            "fas",
+            "fa-exclamation-triangle",
+            "warning-icon"
+          );
+          inputElement.parentNode.insertBefore(
+            warningIcon,
+            inputElement.nextSibling
+          );
+        }
+      } else {
+        inputElement.style.border = "1px solid #d8d6de";
+        const warningIcon = inputElement.querySelector(".warning-icon");
+        if (warningIcon) {
+          warningIcon.remove();
+        }
+      }
+    });
+  }
+*/
