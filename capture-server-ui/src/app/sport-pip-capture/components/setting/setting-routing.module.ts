4836231ec88a0702747b2576e4a605ac4db72d63
rakesh <rakesh.ranjan@drake.co.in>
2024-05-14 11:34:14 +0530

    pull error

--- capture-server-ui/src/app/sport-pip-capture/components/setting/setting-routing.module.ts
+++ capture-server-ui/src/app/sport-pip-capture/components/setting/setting-routing.module.ts
@@ -1,37 +1,35 @@
-import { NgModule } from '@angular/core';
-import { SettingComponent } from './setting.component';
-import { AccountComponent } from './account/account.component';
-import { FilesComponent } from './files/files.component';
-import { SharingComponent } from './sharing/sharing.component';
-import { PreferencesComponent } from './preferences/preferences.component';
-import { LiceneseComponent } from './licenese/licenese.component';
-import { AccountPresenter } from './account/account.presenter';
-import { SharingPresenter } from './sharing/sharing.presenter';
-import { FilePresenter } from './files/files.presenter';
-import { RouterModule, Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { MediaServerComponent } from './media-server/media-server.component';
-
+import { NgModule } from "@angular/core";
+import { SettingComponent } from "./setting.component";
+import { AccountComponent } from "./account/account.component";
+import { FilesComponent } from "./files/files.component";
+import { SharingComponent } from "./sharing/sharing.component";
+import { PreferencesComponent } from "./preferences/preferences.component";
+import { LiceneseComponent } from "./licenese/licenese.component";
+import { AccountPresenter } from "./account/account.presenter";
+import { SharingPresenter } from "./sharing/sharing.presenter";
+import { FilePresenter } from "./files/files.presenter";
+import { RouterModule, Routes } from "@angular/router";
+import { UserComponent } from "./user/user.component";
+import { MediaServerComponent } from "./media-server/media-server.component";
 
 const routes: Routes = [
   {
-    path: ':settings', component: SettingComponent,
+    path: ":settings",
+    component: SettingComponent,
     children: [
-      { path: 'account', component: AccountPresenter },
-      { path: 'file', component: FilePresenter },
-      { path: 'user', component: UserComponent },
-      { path: 'sharing', component: SharingPresenter },
-      { path: 'preferences', component: PreferencesComponent },
-      { path: 'licenese', component: LiceneseComponent },
-      {path:'mediaserver',component:MediaServerComponent}
-
-    ]
+      { path: "account", component: AccountPresenter },
+      { path: "file", component: FilePresenter },
+      { path: "user", component: UserComponent },
+      { path: "sharing", component: SharingPresenter },
+      { path: "preferences", component: PreferencesComponent },
+      { path: "licenese", component: LiceneseComponent },
+      { path: "mediaserver", component: MediaServerComponent },
+    ],
   },
-
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SettingRoutingModule { }
+export class SettingRoutingModule {}
