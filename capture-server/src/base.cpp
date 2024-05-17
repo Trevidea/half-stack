933866e75e02297ee74f96a69caffb0dcd431daf
Shrerya <shreya@drake.co.in>
2024-05-17 16:06:50 +0530

    commit

--- capture-server/src/base.cpp
+++ capture-server/src/base.cpp
@@ -1,10 +1,10 @@
-//base.cpp
+// base.cpp
 #include "base.h"
 #include "gateway.h"
 #include <json/json.h>
 #include "db-manager.h"
 
-Base::Base(Json::Value& model):m_model{model}
+Base::Base(Json::Value &model) : m_model{model}
 {
 }
 
@@ -19,6 +19,10 @@ std::string Base::toString()
 
 std::string Base::toResponse()
 {
+    // Log the content of m_model for debugging purposes
+    std::string modelString = Json::FastWriter().write(this->m_model);
+    spdlog::debug("Model content in toResponse(): {}", modelString);
+
     return Gateway::instance().formatResponse({{this->m_model}});
 }
 
