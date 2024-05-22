9a2ae1730359bdc2799696e439e228c10dd768ca
Manish Verma <manish@drake.in>
2024-05-22 19:55:51 +0530

    changes to model reversed

--- capture-server/pg-dal/model.h
+++ capture-server/pg-dal/model.h
@@ -1,27 +1,24 @@
 #ifndef MODEL_H
 #define MODEL_H
-
 #include <iostream>
 #include "json/json.h"
 #include "spdlog/spdlog.h"
+
 
 class Model
 {
 private:
     Json::Value m_result;
 
+
 public:
     Model() = default;
     Model(const Json::Value &js);
 
     template<typename T>
-    T get(const std::string &field) const;
-
-    // Specialization for Json::Value
-    template<>
-    Json::Value get<Json::Value>(const std::string &field) const
+    T get(const std::string &field) const
     {
-        return m_result[field];
+        return m_result[field].as<T>();
     }
 
     template<typename T>
@@ -29,7 +26,6 @@ public:
     {
         this->m_result[field] = val;
     }
-
     void merge(Model &model);
     Json::Value populate(Json::Value &templ) const;
     std::string toString() const
@@ -37,15 +33,8 @@ public:
         Json::FastWriter fw;
         return fw.write(this->m_result);
     }
-
     ~Model();
 };
 
-// Template method definition
-template<typename T>
-T Model::get(const std::string &field) const
-{
-    return m_result[field].as<T>();
-}
 
 #endif // MODEL_H
