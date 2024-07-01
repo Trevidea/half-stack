3a6d53adf36fbcd8544cb493a30f54a3bd7431ac
Manish Verma <manish@drake.in>
2024-07-01 08:25:41 +0530

    added api for tagging

--- capture-server/src/tagging-engine.cpp
+++ capture-server/src/tagging-engine.cpp
@@ -1,6 +1,6 @@
 #include "tagging-engine.h"
 #include "gateway.h"
-#include "json-driver.h"
+#include "tag-json.h"
 
 TaggingEngine::TaggingEngine(/* args */)
 {
@@ -11,9 +11,26 @@ TaggingEngine::~TaggingEngine()
 }
 void TaggingEngine::report()
 {
-    Gateway::instance().route("GET", "/api/tagging-engine", // Ensure the correct endpoint is handled
+    Gateway::instance().route("POST", "/api/tagging-engine/mark", // Ensure the correct endpoint is handled
                               [this](const Request &req, Response &rsp)
                               {
-                                JsonDriver jd;
+
+                                  std::string json_str = req.data();
+                                  TagJson tag{"."};
+                                  tag.mark(json_str);
+                                  if (tag.err().empty())
+                                  {
+                                      Json::Value result = Json::objectValue;
+                                      result["path"] = tag.path();
+                                      auto strResponse = Gateway::instance().formatResponse({{result}});
+                                      rsp.setData(strResponse);
+                                  }
+                                  else
+                                  {
+                                      Json::Value result = Json::objectValue;
+                                      result["err"] = tag.err();
+                                      auto strResponse = Gateway::instance().formatResponse({{result}});
+                                      rsp.setError(tag.err());
+                                  }
                               });
 }
