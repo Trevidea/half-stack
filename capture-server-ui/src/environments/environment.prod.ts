46ab878a0b7746d76a2edcfaf9ea6f67a273f1db
Sanjay Singh Rawat <sanjay@drake.co.in>
2024-09-27 17:35:18 +0530

    changes made in env prod

--- capture-server-ui/src/environments/environment.prod.ts
+++ capture-server-ui/src/environments/environment.prod.ts
@@ -10,4 +10,5 @@ export const environment = {
   rtmpUrl: "rtmp://drake.in:1935",
   playerUrl: "https://drake.in:3334",
   keycloakUrl: 'https://keycloak.drake.in:5555',
+  keycloakRedirectUrl: 'http://localhost:4200',
 };
