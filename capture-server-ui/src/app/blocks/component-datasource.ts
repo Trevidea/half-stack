edf7c893b7bde3f032cc617469c8245f558a70f9
Sanjay Singh Rawat <sanjay@drake.co.in>
2024-10-08 18:23:28 +0530

    changes made in capture-server-ui

--- capture-server-ui/src/app/blocks/component-datasource.ts
+++
@@ -1,5 +0,0 @@
-export class ComponentDatasource {
-    refresh(){
-        console.log("Refresh called")
-    }
-}
--- capture-server-ui/src/app/sport-pip-capture/components/event/event-utility/event-ui-interface.ts
+++
@@ -1,8 +0,0 @@
-export namespace UI {
-    export interface DropDownMenuItem {
-        action: () => void;
-        icon: string;
-        label: string;
-        type?: 'feather' | 'bootstrap' | 'svg';
-    }
-}
