63a9c5aeb549c91a158f311b9e18697e1706d572
Sanjay Singh Rawat <sanjay@drake.co.in>
2024-10-14 19:08:55 +0530

    chnages made in add device to event components

--- capture-server-ui/src/app/pages/event/add-device-to-event/add-device-to-event.component.ts
+++ capture-server-ui/src/app/pages/event/add-device-to-event/add-device-to-event.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MaterialModule } from 'src/app/material.module';
 
 @Component({
   selector: 'app-add-device-to-event',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,],
   templateUrl: './add-device-to-event.component.html',
   styleUrl: './add-device-to-event.component.scss'
 })
--- capture-server-ui/src/app/pages/event/event-preview/active-devices/active-devices.component.ts
+++ capture-server-ui/src/app/pages/event/event-preview/active-devices/active-devices.component.ts
@@ -1,18 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MaterialModule } from 'src/app/material.module';
 import { UI } from 'src/app/pages/blocks/ui-interface';
 
 @Component({
   selector: 'app-active-devices',
   standalone: true,
-  imports: [CommonModule,MaterialModule],
+  imports: [CommonModule, MaterialModule],
   templateUrl: './active-devices.component.html',
   styleUrl: './active-devices.component.scss'
 })
 export class ActiveDevicesComponent {
   @Input() eventId: any;
   @Input() datasource: any;
+  @Output() onAddDeviceToevent = new EventEmitter();
   dropdownItems: UI.DropDownMenuItem[] = [
     { label: 'Block', icon: 'slash', action: () => { } },
     { label: 'Remove', icon: 'trash', action: () => { } },
