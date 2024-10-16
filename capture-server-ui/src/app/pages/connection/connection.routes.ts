275660daf370732d3cf55ad896dec6e843ad75e1
Rakesh <rakesh.ranjan@drake.co.in>
2024-10-16 19:02:53 +0530

    Working on connection

--- capture-server-ui/src/app/pages/connection/connection.routes.ts
+++ capture-server-ui/src/app/pages/connection/connection.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ConnectionComponent } from './connection.component';
-import { LiveEventConnectionComponent } from './live-event-connection/live-event-connection.component';
 import { ConnectionDeviceDetailPresenter } from './connection-device-detail/connection-device-detail.presenter';
 import { LiveEventConnectionPresenter } from './live-event-connection/live-event-connection.presenter';
 import { StreamingConfigurationComponent } from './connection-device-detail/streaming-configuration/streaming-configuration.component';
